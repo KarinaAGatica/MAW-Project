0310b5dc8f921e3b458718a5f41afa362da6497e
KarinaGatica <karii.gatica@gmail.com>
2018-05-16 17:17:01 -0300

    Se realizo la vista Como Ayudar
    Se agrego aparte los sponsors

--- js/menumaker.js
+++ js/menumaker.js
@@ -1,11 +1,11 @@
 (function($) {
 
   $.fn.menumaker = function(options) {
-      
+
       var cssmenu = $(this), settings = $.extend({
         title: "Menu",
         format: "dropdown",
-        breakpoint: 768,
+        breakpoint: 908,
         sticky: false,
       }, options);
 
@@ -16,7 +16,7 @@
           $(this).find("#menu-button").on('click', function(){
             $(this).toggleClass('menu-opened');
             var mainmenu = $(this).next('ul');
-            if (mainmenu.hasClass('open')) { 
+            if (mainmenu.hasClass('open')) {
               mainmenu.hide().removeClass('open');
             }
             else {
@@ -97,4 +97,3 @@
    title: "Menu",
    format: "multitoggle"
 });
-
